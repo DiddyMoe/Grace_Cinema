10ba25957160e6397b5011062362ea4a49a8d1dd
Mohammad Saad <msaad10792@gmail.com>
2023-05-23 12:27:07 -0500

    Updated and refactored server/db/models/Movie.js

--- server/db/models/Movie.js
+++ server/db/models/Movie.js
@@ -1,46 +1,60 @@
+// Importing the Sequelize module and database instance
 const Sequelize = require('sequelize');
 const db = require('../_db');
 
+// Defining the Movie model
 const Movie = db.define('movie', {
-    title: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    genre: {
-			//⬇️DON'T UPDATE THE INPUT STRING ON ENUM WITHOUT ALSO CHANGING THE `allowedGenres` ARRAY ON ./src/components/AddMovie.js &AND& ./src/components/EditMovie.js❗️
-        type: Sequelize.ENUM('Action/Adventure', 'Biography/Historical', 'Comedy', 'Documentary', 'Drama', 'Family/Animated', 'Fantasy', 'Horror/Thriller', 'Romance', 'Science Fiction', 'Western'),
-			//⬆️DON'T UPDATE THE INPUT STRING ON ENUM WITHOUT ALSO CHANGING THE `allowedGenres` ARRAY ON ./src/components/AddMovie.js &AND& ./src/components/EditMovie.js❗️	
-        allowNull: false,
-    },
-    year: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        validate: {
-          min: 1888
-        }
-    },
-    description: {
-        type: Sequelize.TEXT,
-        allowNull: false,
-    },
-    price: {
-      type: Sequelize.DECIMAL(10, 2),
-      allowNull: false,
-      validate: {
-        min: .01
-      }
-  },
-  inventory: {
-    type: Sequelize.INTEGER,
-		defaultValue: 0,
-    allowNull: false,
-    validate: {
-      min: 0
-    }
-	},
-	imageUrl: {
-		type: Sequelize.STRING,
-	}
+ title: {
+ type: Sequelize.STRING,
+ allowNull: false,
+ },
+ genre: {
+ // Enum values for the genre field
+ type: Sequelize.ENUM(
+ 'Action/Adventure',
+ 'Biography/Historical',
+ 'Comedy',
+ 'Documentary',
+ 'Drama',
+ 'Family/Animated',
+ 'Fantasy',
+ 'Horror/Thriller',
+ 'Romance',
+ 'Science Fiction',
+ 'Western'
+ ),
+ allowNull: false,
+ },
+ year: {
+ type: Sequelize.INTEGER,
+ allowNull: false,
+ validate: {
+ min: 1888,
+ },
+ },
+ description: {
+ type: Sequelize.TEXT,
+ allowNull: false,
+ },
+ price: {
+ type: Sequelize.DECIMAL(10, 2),
+ allowNull: false,
+ validate: {
+ min: 0.01,
+ },
+ },
+ inventory: {
+ type: Sequelize.INTEGER,
+ defaultValue: 0,
+ allowNull: false,
+ validate: {
+ min: 0,
+ },
+ },
+ imageUrl: {
+ type: Sequelize.STRING,
+ },
 });
 
+// Exporting the Movie model
 module.exports = Movie;
