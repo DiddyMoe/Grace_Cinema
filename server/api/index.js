b236f4bc8656fde033bf6c55f4e962c61562e088
Mohammad Saad <msaad10792@gmail.com>
2023-05-23 16:36:48 -0500

    Added comments to explain what each line of code does in server/api/index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,17 @@
 const router = require('express').Router()
 
+// Mount the personnel routes on the /personnel path
 router.use('/personnel', require('./personnel'));
+
+// Mount the movies routes on the /movies path
 router.use('/movies', require('./movies'));
+
+// Mount the users routes on the /users path
 router.use('/users', require('./users'));
+
+// Mount the orders routes on the /orders path
 router.use('/orders', require('./orders'));
 
-//add additional model routes here as you create them!
+// Add additional model routes here as you create them!
 
 module.exports = router;
