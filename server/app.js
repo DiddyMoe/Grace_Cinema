f49477df60fff5029220fbab6c6e23ce13575035
Mohammad Saad <msaad10792@gmail.com>
2023-05-23 12:17:35 -0500

    UJpdated and refactored server/app.js

--- server/app.js
+++ server/app.js
@@ -1,14 +1,13 @@
+// Setting the port number
 const PORT = 8080;
+
+// Importing the server instance and database object
 const server = require("./index");
 const { db } = require("./db");
 
-
-
+// Syncing the database and starting the server
 db.sync().then(() => {
-    server.listen(PORT, () =>
-        console.log(`
-        Listening on port ${PORT}
-        http://localhost:${PORT}/
-    `)
-    );
+ server.listen(PORT, () => {
+ console.log(`Listening on port ${PORT}\nhttp://localhost:${PORT}/`);
+ });
 });
