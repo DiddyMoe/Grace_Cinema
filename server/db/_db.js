9f1c3d0cbc7b512ea9fd12fe2b23cc60b0dff14d
Mohammad Saad <msaad10792@gmail.com>
2023-05-23 12:23:13 -0500

    Updated and refactored server/db/_db.js

--- server/db/_db.js
+++ server/db/_db.js
@@ -1,21 +1,27 @@
+// Importing the Sequelize module
 const Sequelize = require('sequelize');
 
-
+// Setting the configuration options for the database connection
 const config = {
-  logging: false,
-}
- if(process.env.LOGGING){
-  delete config.logging;
- }
+ logging: false,
+};
 
- if(process.env.DATABASE_URL){
-  config.dialectOptions = {
-    ssl: {
-      rejectUnauthorized: false
-    }
-  };
+// Enabling logging if the LOGGING environment variable is set
+if (process.env.LOGGING) {
+ delete config.logging;
 }
 
-const db = new Sequelize(process.env.DATABASE_URL ||'postgres://localhost:5432/grace_cinema', config);
+// Setting SSL options if the DATABASE_URL environment variable is set
+if (process.env.DATABASE_URL) {
+ config.dialectOptions = {
+ ssl: {
+ rejectUnauthorized: false,
+ },
+ };
+}
 
+// Creating a new instance of Sequelize with the given database URL and configuration options
+const db = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost:5432/grace_cinema', config);
+
+// Exporting the database instance
 module.exports = db;
