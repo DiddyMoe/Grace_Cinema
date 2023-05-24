f1fb47d225f9f03d8f3bcf7639ffb9184f6d847b
Mohammad Saad <msaad10792@gmail.com>
2023-05-24 18:45:32 -0500

    Updated and refactored src/Personnel.js

--- src/components/Personnel.js
+++ src/components/Personnel.js
@@ -1,38 +1,24 @@
 import React from "react";
-import Nav from "./Nav";
-import Movies from "./Movies";
-import SingleMovie from "./SingleMovie";
-import SinglePerson from "./SinglePerson";
-import Admin from "./Admin";
-import UserProfile from "./UserProfile";
-import Users from "./Users";
-import Cart from "./Cart";
-import Orders from "./Orders";
-import SideNav from "./SideNav";
-import NoPage from "./NoPage";
-
-// HOLD THE INFORMATION ABOUT THE MOVIES POEPLE (EX: ACTORS, DIRECTORS)
 import { useSelector } from "react-redux";
 import { selectPersonnel } from '../features/allPersonnelSlice';
 
+// Personnel component displays information about movie personnel (e.g. actors, directors)
 const Personnel = () => {
-	const persons = useSelector(selectPersonnel);
-	console.log(persons);
-	return (
-		<div>
-		{
-			persons.map((person)=>{
-				return(
-					<div key = {person.fName}>
-						<h4>Name: {person.fName} {person.lName}</h4>
-						<h4> details: {person.details}</h4>
-						<img src = {person.imageUrl} alt=''/>
-					</div>
-				)
-			})
-		}
-	</div>
-		);
+  // Get personnel data from Redux store
+  const persons = useSelector(selectPersonnel);
+
+  // Render personnel information
+  return (
+    <div>
+      {persons.map((person) => (
+        <div key={person.fName}>
+          <h4>Name: {person.fName} {person.lName}</h4>
+          <h4>Details: {person.details}</h4>
+          <img src={person.imageUrl} alt='' />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Personnel;
