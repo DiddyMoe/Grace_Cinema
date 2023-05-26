d5a7bf2c76b1f0d57cf0a18ac6eb6f2a54406172
Mohammad Saad <msaad10792@gmail.com>
2023-05-26 12:09:53 -0500

    Further optimized allMoviesSlice.js, I hope this is the industry standard method of doing this api call

--- src/features/allMovies/allMoviesSlice.js
+++ src/features/allMovies/allMoviesSlice.js
@@ -1,13 +1,18 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+// Create an axios instance with the base URL pre-configured
+const api = axios.create({
+  baseURL: `http://localhost:8080/api/movies`,
+});
+
 // Initial state for the movies slice
 const initialState = [];
 
 // Async thunk to fetch movies from the API
 export const fetchMoviesAsync = createAsyncThunk("movies", async () => {
   try {
-    const { data } = await axios.get(`http://localhost:8080/api/movies`);
+    const { data } = await api.get("/");
     return data;
   } catch (err) {
     console.log(err);
@@ -16,19 +21,19 @@ export const fetchMoviesAsync = createAsyncThunk("movies", async () => {
 
 // Async thunk to add a movie to the API
 export const addMovieAsync = createAsyncThunk("addMovie", async (movie) => {
-  const { data } = await axios.post(`/api/movies`, movie);
+  const { data } = await api.post("/", movie);
   return data;
 });
 
 // Async thunk to edit a movie in the API
 export const editMovieAsync = createAsyncThunk("editMovie", async (movie) => {
-  const { data } = await axios.put(`/api/movies/${movie.id}`, movie);
+  const { data } = await api.put(`/${movie.id}`, movie);
   return data;
 });
 
 // Async thunk to delete a movie from the API
 export const deleteMovieAsync = createAsyncThunk("deleteMovie", async (id) => {
-  await axios.delete(`http://localhost:8080/api/movies/${id}`);
+  await api.delete(`/${id}`);
   return id;
 });
 
@@ -48,8 +53,10 @@ const moviesSlice = createSlice({
     });
     // Handle fulfilled editMovieAsync action
     builder.addCase(editMovieAsync.fulfilled, (state, action) => {
-      const movies = state.filter((movie) => movie.id !== action.payload);
-      state = [...movies, action.payload];
+      const index = state.findIndex((movie) => movie.id === action.payload.id);
+      if (index !== -1) {
+        state[index] = action.payload;
+      }
     });
     // Handle fulfilled deleteMovieAsync action
     builder.addCase(deleteMovieAsync.fulfilled, (state, action) => {
