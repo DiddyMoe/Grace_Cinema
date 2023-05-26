409b8572200287317187afd797ef6cb7b5bd0c57
Mohammad Saad <msaad10792@gmail.com>
2023-05-26 14:32:40 -0500

    Refactored more code

--- src/features/allPersonnelSlice.js
+++ src/features/allPersonnelSlice.js
@@ -1,38 +1,37 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
+// Define the initial state for the personnel slice
 const initialState = [];
 
-export const fetchPersonnelAsync = createAsyncThunk("personnel", async () => {
-  try {
-    const { data } = await axios.get(`http://localhost:8080/api/personnel`);
-    return data;
-  } catch (err) {
-    console.log(err);
+// Define an asynchronous thunk action to fetch personnel data from the API
+export const fetchPersonnelAsync = createAsyncThunk(
+  "personnel/fetchPersonnel",
+  async () => {
+    try {
+      const { data } = await axios.get(`http://localhost:8080/api/personnel`);
+      return data;
+    } catch (err) {
+      console.log(err);
+    }
   }
-});
+);
 
-// export const addMovieAsync = createAsyncThunk("addMovie", async (movie) => {
-//     const { data } = await axios.post(`/api/Personnel`, movie);
-//     return data
-// });
-
+// Define a slice for the personnel data
 const personnelSlice = createSlice({
   name: "personnel",
   initialState,
   reducers: {},
   extraReducers: (builder) => {
+    // Handle the fulfilled state of the fetchPersonnelAsync action
     builder.addCase(fetchPersonnelAsync.fulfilled, (state, action) => {
       return action.payload;
     });
-    // builder.addCase(addMovieAsync.fulfilled, (state, action) => {
-    //   state.push(action.payload);
-    // });
   },
 });
 
-export const selectPersonnel = (state) => {
-  return state.personnel;
-};
+// Define a selector to get the personnel data from the state
+export const selectPersonnel = (state) => state.personnel;
 
+// Export the reducer as the default export
 export default personnelSlice.reducer;
--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -1,75 +1,71 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-
 const TOKEN = 'token';
 
+// Async thunk to get user information
 export const me = createAsyncThunk("auth/me", async () => {
-  try {
-const token = window.localStorage.getItem(TOKEN);
-		if (token) {
-			const res = await axios.get('/auth/me', {
-				headers: {
-					authorization: token,
-				},
-			});
-      return res.data;
-		} else {
-      return {};
-    }
-  } catch (err) {
-    if(err.response.data) {
-      return thunkAPI.rejectWithValue(err.response.data);
-    } else {
-      return 'Theres an issue with your request'
-    }
+ try {
+  const token = window.localStorage.getItem(TOKEN);
+  if (token) {
+   const res = await axios.get('/auth/me', {
+    headers: {
+     authorization: token,
+    },
+   });
+   return res.data;
+  } else {
+   return {};
   }
+ } catch (err) {
+  if(err.response.data) {
+   return thunkAPI.rejectWithValue(err.response.data);
+  } else {
+   return 'There is an issue with your request'
+  }
+ }
 });
 
-export const authenticate = createAsyncThunk('auth/authenticate', async ({ email
-, password, method }, thunkAPI) => {
-  console.log('working')
-  try{
-    console.log(method, email, password)
-    const res = await axios.post(`/auth/${method}`, { email, password });
-    window.localStorage.setItem(TOKEN, res.data.token);
-    thunkAPI.dispatch(me());
-
-  }catch(err) {
-    console.log('working3')
-    if (err.response.data) {
-      return thunkAPI.rejectWithValue(err.response.data);
-    } else {
-      return 'There was an issue with your request.';
-    }
+// Async thunk to authenticate user
+export const authenticate = createAsyncThunk('auth/authenticate', async ({ email, password, method }, thunkAPI) => {
+ try{
+  const res = await axios.post(`/auth/${method}`, { email, password });
+  window.localStorage.setItem(TOKEN, res.data.token);
+  thunkAPI.dispatch(me());
+ }catch(err) {
+  if (err.response.data) {
+   return thunkAPI.rejectWithValue(err.response.data);
+  } else {
+   return 'There was an issue with your request.';
   }
+ }
 })
 
-
+// Slice to handle authentication state
 export const authSlice = createSlice({
-  name: 'auth',
-  initialState: {
-    me: {},
-    error: null,
+ name: 'auth',
+ initialState: {
+  me: {},
+  error: null,
+ },
+ reducers: {
+  logout(state, action) {
+   window.localStorage.removeItem(TOKEN);
+   state.me = {};
+   state.error = null;
   },
-  reducers: {
-    logout(state, action) {
-      window.localStorage.removeItem(TOKEN);
-      state.me = {};
-      state.error = null;
-    },
-  },
-  extraReducers: (builder) => {
-    builder.addCase(me.fulfilled, (state, action) => {
-      state.me = action.payload;
-    });
-    builder.addCase(me.rejected, (state, action) => {
-      state.error = action.error;
-    });
-    builder.addCase(authenticate.rejected, (state, action) => {
-      state.error = action.payload;
-    });
-  },
+ },
+ extraReducers: (builder) => {
+  builder.addCase(me.fulfilled, (state, action) => {
+   state.me = action.payload;
+  });
+  builder.addCase(me.rejected, (state, action) => {
+   state.error = action.error;
+  });
+  builder.addCase(authenticate.rejected, (state, action) => {
+   state.error = action.payload;
+  });
+ },
 });
 
 export const { logout } = authSlice.actions;
